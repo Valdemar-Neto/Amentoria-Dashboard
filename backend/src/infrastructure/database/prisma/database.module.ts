84acc21af986292b936025ada302d4af9e2fea20
Valdemar Neto <valldemar@live.com>
2026-01-28 15:24:02 -0300

    refactor: add new module

--- backend/src/infrastructure/database/prisma/database.module.ts
+++ backend/src/infrastructure/database/prisma/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";    
 import { PrismaService } from "./prisma.service";
 import { PrismaStudentRepository } from "./repositories/prisma-student-repository";
+import { PrismaDashboardRepository } from "./repositories/prisma-dashboard-repository";
 
 
 //Responsavel por entregar um repositorio de estudantes
@@ -8,15 +9,23 @@ import { PrismaStudentRepository } from "./repositories/prisma-student-repositor
 @Module({
     providers:[
         PrismaService,
+        //repositorio de alunos
         {
             provide: 'IStudentsRepository',
             useClass: PrismaStudentRepository,
         },
+
+        //repositorio de dashboard
+        {
+            provide: 'IDashboardRepository',
+            useClass: PrismaDashboardRepository,
+        }
     ],
 
     exports: [
         PrismaService,
         'IStudentsRepository',
+        'IDashboardRepository',
     ],
 })
 
