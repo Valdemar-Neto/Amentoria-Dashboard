1be96f17e5597ed121ff25fb24e16d6e4ef22a3b
Valdemar Neto <valldemar@live.com>
2026-01-28 13:10:10 -0300

    docs: include new comments

--- backend/src/infrastructure/database/prisma/prisma.service.ts
+++ backend/src/infrastructure/database/prisma/prisma.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from '@prisma/client';
 
+
+// Responsavel por conectar o banco de dados como backend
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
     async onModuleInit() {
